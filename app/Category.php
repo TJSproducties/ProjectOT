edd3b2036fa826fe98942e261558948f57c20e01
olevanderheiden <43372501+olevanderheiden@users.noreply.github.com>
2020-09-29 14:27:35 +0200

    Fix spatie permission provider

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function book() {
+        return $this->hasMany(Book::class);
+    }
+
 }
+
+
--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,9 +4,12 @@
 
 use App\Category;
 use Faker\Generator as Faker;
+use App\Book;
 
 $factory->define(Category::class, function (Faker $faker) {
     return [
-        //
+        'price'=>$faker->randomFloat(2,2,6),
+        'effdata'=>$faker->dateTime,
+        'book_id'=> Book::all()->random()->id,
     ];
 });
