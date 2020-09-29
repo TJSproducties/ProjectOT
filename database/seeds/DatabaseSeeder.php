c30bd32993d27ddb0ab352990f79922514245dd5
olevanderheiden <43372501+olevanderheiden@users.noreply.github.com>
2020-09-29 11:57:29 +0200

    Fix spatie permission provider

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        app()['cache']->forget('spatie.permission.cache');
+        Permission::create(['name'=>'create category']);
+        Permission::create(['name'=>'edit category']);
+        Permission::create(['name'=>'delete category']);
+        Permission::create(['name'=>'create books']);
+        Permission::create(['name'=>'edit books']);
+        Permission::create(['name'=>'delete books']);
+        $role = Role::create(['name'=>'customer']);
+        $role = Role::create(['name'=>'sales']);
+        $role->givePermissionTo('create category','edit category');
+        $role = Role::create(['name'=>'admin']);
+        $role->givePermissionTo(Permission::all());
     }
 }
