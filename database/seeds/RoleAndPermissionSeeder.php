d7ea0ab05ec09653b0ebe4d38472a77f7ff6f929
olevanderheiden <43372501+olevanderheiden@users.noreply.github.com>
2020-09-29 11:27:55 +0200

    Fix spatie permission provider

--- database/seeds/RoleAndPermissionSeeder.php
+++ database/seeds/RoleAndPermissionSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RoleAndPermissionSeeder extends Seeder
 {
@@ -11,6 +13,21 @@ class RoleAndPermissionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        app()['cache']->forget('spatie.permission.cache');
+        Permission::create(['name'=>'create category']);
+        Permission::create(['name'=>'edit category']);
+        Permission::create(['name'=>'delete category']);
+        Permission::create(['name'=>'create books']);
+        Permission::create(['name'=>'edit books']);
+        Permission::create(['name'=>'delete books']);
+
+        $role = Role::create(['name'=>'customer']);
+
+        $role = Role::create(['name'=>'sales']);
+        $role->givePermissionTo('create category','edit category');
+
+        $role = Role::create(['name'=>'admin']);
+        $role->givePermissionTo(Permission::all());
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/categories','CategoryController@index')->name('categories');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
