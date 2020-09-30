4a5118356a09cc119dc77393dddf69981766bb29
TJSproducties <tjsprodukties@gmail.com>
2020-09-30 15:57:10 +0200

    Categories.index categories.create categories.edit categories.show categories.delete web.php routing in progres

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -72,6 +72,6 @@
         <span class="text-muted">OJVDH & T.Schenderling©.</span>
     </div>
 </footer>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script></body>
+<script src="{{asset('js/app.js')}}"></script>
+</body>
 </html>
