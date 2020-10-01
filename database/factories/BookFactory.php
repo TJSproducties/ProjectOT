544c8a73b0db60da6af65e033ae3e3bc3b46f29e
TJSproducties <tjsprodukties@gmail.com>
2020-10-01 13:21:02 +0200

    All Category pages will work now changes in the navbar showing the categories and books

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -11,7 +11,7 @@ $factory->define(Book::class, function (Faker $faker) {
         //
         'title' => $faker->title,
         'description' => $faker->paragraph(15),
-        'isbn' => $faker->randomFloat(0, 25, 25),
+        'isbn' => $faker->isbn13,
         'category_id' => Category::all()->random()->id
 
 
