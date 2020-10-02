bd4fa2db80fc0ed5f2ef0e52218bb6379d1064f0
olevanderheiden <43372501+olevanderheiden@users.noreply.github.com>
2020-10-02 10:42:43 +0200

    All users with roles created

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -20,5 +20,21 @@ class UserSeeder extends Seeder
                 $user->assignRole('customer');
             });
 
+        factory(User::class, 1)->create([
+            'name'=>'sales',
+            'email'=>'sales@test.com',
+            'password'=>bcrypt('test1234')])
+            ->each(function (User $user){
+                $user->assignRole('sales');
+            });
+
+        factory(User::class, 1)->create([
+            'name'=>'admin',
+            'email'=>'admin@test.com',
+            'password'=>bcrypt('test1234')])
+            ->each(function (User $user){
+                $user->assignRole('admin');
+            });
+
     }
 }
